9dd8ac12fc34da80fba53d362c7fc52fcf7d7d8d
Tri Nhan <trinhansg2020@gmail.com>
2021-01-23 13:26:54 +0700

    u

--- scripts/trinhansg_ex.js
+++ scripts/trinhansg_ex.js
@@ -8,14 +8,24 @@ function load_page(index){
 	$("#content").load(`page${index}.html`, function() {});
 }
 function load_page_ex(index){
+	page_size = 8
+	start_index = (index - 1)*page_size + 1
+	end_index = start_index + page_size
 	content = ""
-	for(i=1;i<=8;i++){
+	for(i=start_index;i<end_index;i++){
 		content +=
 			`<div class="col-sm-6">
 				<div class="aspect-ratio-container aspect-ratio-4-3">
-					<img src="page${index}/h${i}.jpg" class="aspect-ratio-object"/>
+					<img src="h${i}.jpg" class="aspect-ratio-object"/>
 				</div>			
 			</div>`
 	}
 	$("#content").html(content);
+}
+var images = [];
+function preload_images(num_of_images){
+	for (var i = 1; i <= num_of_images; i++) {
+        images[i] = new Image();
+        images[i].src = "h${i}.jpg";
+    }
 }
