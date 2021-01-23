a1b8efad8f420e3feb9e752a26492309ee7d43f3
Tri Nhan <trinhansg2020@gmail.com>
2021-01-23 12:20:05 +0700

    u

--- scripts/trinhansg.js
+++ scripts/trinhansg.js
@@ -8,13 +8,9 @@ $("#footer").load("https://trinhansg.github.io/footer.html", function() {
   //alert( "Load was performed." );
 });
 function load_page(index){
-	$("#content").load(`page${index}.html`, function() {
-	  //alert( "Load was performed." );
-	});
+	$("#content").load(`page${index}.html`, function() {});
 }
 function load_page_ex(index){
 	$("#imgpath").attr("href",`page${index}`)
-	$("#content").load(`page1.html`, function() {
-	  
-	});
+	$("#content").load(`page1.html`, function() {alert( `Load was performed ${index}` );});
 }
